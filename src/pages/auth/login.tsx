546d778bc4de845f99f92b044e90526872107dab
Ermiyas Zeleke <78529847+ermi2021@users.noreply.github.com>
2023-11-05 16:18:16 +0300

    added validation to the login form

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -9,13 +9,50 @@ import {
   Button,
   HStack,
   Spacer,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as yup from "yup";
+
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPasword, setShowPassword] = useState(false);
+
+  const LoginSchema = yup.object().shape({
+    username: yup.string().required(),
+    password: yup.string().required("password is required"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (values) => {
+      setIsLoading(true);
+      setTimeout(() => {
+        if (formik.isValid) {
+          setIsLoading(false);
+        } else {
+          setIsLoading(false);
+        }
+      }, 2000);
+    },
+  });
   return (
     <Box bg={"#f1f1f1"} width="100%" height="100vh" overflow={"hidden"}>
       <AbsoluteCenter>
-        <Box bg={"white"} p={5} borderRadius={"lg"} w={"50vh"} boxShadow={"lg"} dropShadow={'lg'}>
+        <Box
+          bg={"white"}
+          p={5}
+          borderRadius={"lg"}
+          w={"50vh"}
+          boxShadow={"lg"}
+          dropShadow={"lg"}
+        >
           <Divider
             orientation="horizontal"
             color={"black"}
@@ -23,15 +60,56 @@ const Login = () => {
             height={"0.5"}
           />
           <Stack spacing={4}>
-            <Box marginX={'auto'} alignContent={'center'} alignItems={'center'} justifyContent={'center'}>
-              <Text fontSize="xl" fontWeight={"bold"} >
+            <Box>
+              <Text fontSize="xl" fontWeight={"bold"}>
                 Login
               </Text>
-              <Text fontSize="sm" textAlign={'center'}>Please enter your username and password</Text>
+              <Text fontSize="sm">Please enter your username and password</Text>
             </Box>
-            <Input placeholder="Username" size="md" />
-            <Input placeholder="Password" size="md" />
-            <Button colorScheme={"teal"} variant={"solid"}>
+            <Input
+              isInvalid={formik.errors.username ? true : false}
+              errorBorderColor="red.200"
+              borderWidth={"thin"}
+              focusBorderColor="gray.300"
+              borderColor={"gray.400"}
+              name="username"
+              placeholder="Username"
+              size="md"
+              onChange={formik.handleChange("username")}
+            />
+            <InputGroup size="md">
+              <Input
+                name="password"
+                pr="4.5rem"
+                isInvalid={formik.errors.password ? true : false}
+                errorBorderColor="red.200"
+                focusBorderColor="gray.300"
+                borderColor={"gray.400"}
+                type={showPasword ? "text" : "password"}
+                placeholder="Enter password"
+                onChange={formik.handleChange("password")}
+              />
+              <InputRightElement width="4.5rem">
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  onClick={() => {
+                    setShowPassword(!showPasword);
+                  }}
+                >
+                  {showPasword ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Button
+              isLoading={isLoading}
+              onClick={() => {
+                formik.handleSubmit();
+              }}
+              colorScheme={"teal"}
+              variant={"solid"}
+              fontSize={14}
+            >
               LOGIN
             </Button>
           </Stack>
@@ -47,19 +125,19 @@ const Login = () => {
             </Button>
             <Spacer />
             <Center>
-            <HStack>
-              <Text fontSize={14} fontWeight={"normal"}>
-                Didn't have an account?
-              </Text>
-              <Button
-                colorScheme={"facebook"}
-                variant={"link"}
-                fontSize={14}
-                fontWeight={"normal"}
-              >
-                Sign up
-              </Button>
-            </HStack>
+              <HStack>
+                <Text fontSize={14} fontWeight={"normal"}>
+                  Didn't have an account?
+                </Text>
+                <Button
+                  colorScheme={"facebook"}
+                  variant={"link"}
+                  fontSize={14}
+                  fontWeight={"normal"}
+                >
+                  Sign up
+                </Button>
+              </HStack>
             </Center>
           </Stack>
         </Box>
