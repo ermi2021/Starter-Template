4777109f184a6ff1d67319696b1c84d6e6092c70
Ermiyas Zeleke <78529847+ermi2021@users.noreply.github.com>
2023-11-19 15:04:51 +0300

    wrapper to components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import interceptor from "./services/interceptor"
 import { BrowserRouter } from "react-router-dom";
 import Router from "./pages/router";
 import { ChakraProvider } from "@chakra-ui/react";
+import Wrapper from "./components/wrapper";
 
 function App() {
   return (
