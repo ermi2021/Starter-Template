1b608a78391a98a82f98e03166d1a685204a89ec
Robel <robelalemu2372@gmail.com>
2023-12-06 18:31:18 +0300

    category fix

--- src/components/mobileadvertbanner/index.tsx
+++ src/components/mobileadvertbanner/index.tsx
@@ -1,51 +1,123 @@
-import { Box, Image, GridItem, Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  List,
+  ListItem,
+  Button,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
 import { Carousel } from "react-responsive-carousel";
-import { images } from "../../../src/utils/data/caresoulimages/index";
+import { images2 } from "../../../src/utils/data/caresoulimages/index";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 const MobileAdvertBanner = () => {
   return (
     <Carousel
       showIndicators={false}
       showThumbs={false}
-      autoPlay={true}
+      autoPlay={false}
       infiniteLoop={false}
       showStatus={false}
     >
-      {images.map((image: any) => (
-        <Box height={"fit"} bg={"teal.500"} paddingY={5}>
-          <Box width="85%" margin="auto">
-            <Grid
-              templateColumns={{
-                base: "repeat(1, 1fr)",
-                sm: "repeat(2, 1fr)",
-                md: "repeat(2, 1fr)",
-                lg: "repeat(4, 1fr)",
-                xl: "repeat(4, 1fr)",
-              }}
-              minH="100%"
-              w={"fit"}
-              marginX={10}
-              //   gap={8}
-            >
-              <GridItem>
-                <Box
-                  // flex={{ base: 1, sm: 0.5, md: 0.25, lg: 0.25 }}
-                  //   bg={useColorModeValue("white", "gray.800")}
-                  maxW="sm"
-                  borderWidth="1px"
-                  cursor={"pointer"}
-                  rounded="lg"
-                  shadow="lg"
-                  position="relative"
-                  _hover={{ shadow: "3xl", transition: "all .3s ease" }}
-                >
-                  <Image src={image} rounded="lg" />
-                </Box>
-              </GridItem>
-            </Grid>
-          </Box>
-        </Box>
-      ))}
+      <Box height={"fit"} bg={"teal.500"} paddingY={5}>
+        <List margin={"auto"} width={"90%"}>
+          <SimpleGrid columns={{ base: 3 }}>
+            {images2.map((image: any) => (
+              <ListItem paddingY="15px">
+                <VStack>
+                  <Image
+                    boxSize="70px"
+                    borderRadius={10}
+                    objectFit="contain"
+                    src={image}
+                  />
+
+                  <Button
+                    height={"5"}
+                    fontSize="sm"
+                    color={"white"}
+                    fontWeight={"light"}
+                    variant="unstyled"
+                    textDecoration="none"
+                    whiteSpace="normal"
+                    textAlign="center"
+                    wordBreak={"break-word"}
+                  >
+                    Category
+                  </Button>
+                </VStack>
+              </ListItem>
+            ))}
+          </SimpleGrid>
+        </List>
+      </Box>
+
+      <Box height={"fit"} bg={"teal.500"} paddingY={5}>
+        <List margin={"auto"} width={"90%"}>
+          <SimpleGrid columns={{ base: 3 }}>
+            {images2.map((image: any) => (
+              <ListItem paddingY="15px">
+                <VStack>
+                  <Image
+                    boxSize="70px"
+                    borderRadius={10}
+                    objectFit="contain"
+                    src={image}
+                  />
+
+                  <Button
+                    height={"5"}
+                    fontSize="sm"
+                    color={"white"}
+                    fontWeight={"light"}
+                    variant="unstyled"
+                    textDecoration="none"
+                    whiteSpace="normal"
+                    textAlign="center"
+                    wordBreak={"break-word"}
+                  >
+                    Category
+                  </Button>
+                </VStack>
+              </ListItem>
+            ))}
+          </SimpleGrid>
+        </List>
+      </Box>
+
+      <Box height={"fit"} bg={"teal.500"} paddingY={5}>
+        <List margin={"auto"} width={"90%"}>
+          <SimpleGrid columns={{ base: 3 }}>
+            {images2.map((image: any) => (
+              <ListItem paddingY="15px">
+                <VStack>
+                  <Image
+                    boxSize="70px"
+                    borderRadius={10}
+                    objectFit="contain"
+                    src={image}
+                  />
+
+                  <Button
+                    height={"5"}
+                    fontSize="sm"
+                    color={"white"}
+                    fontWeight={"light"}
+                    variant="unstyled"
+                    textDecoration="none"
+                    whiteSpace="normal"
+                    textAlign="center"
+                    wordBreak={"break-word"}
+                  >
+                    Category
+                  </Button>
+                </VStack>
+              </ListItem>
+            ))}
+          </SimpleGrid>
+        </List>
+      </Box>
     </Carousel>
   );
 };
--- src/utils/data/caresoulimages/index.ts
+++ src/utils/data/caresoulimages/index.ts
@@ -8,3 +8,12 @@ export const images: any = [
   "https://client-int.qtlauncher.com/images/?id=GLX-coinflip_en_US&type=logo-square&version=1695782753222",
   "https://client-int.qtlauncher.com/images/?id=TRB-turboplinko_en_US&type=logo-square&version=1689797661198",
 ];
+
+export const images2: any = [
+  "https://client-int.qtlauncher.com/images/?id=TRB-bayraktar_en_US&type=logo-square&version=1689799195526",
+  "https://client-int.qtlauncher.com/images/?id=1x2-coinfield_en_US&type=logo-square&version=1667798993145",
+  "https://client-int.qtlauncher.com/images/?id=GLX-coinflip_en_US&type=logo-square&version=1695782753222",
+  "https://client-int.qtlauncher.com/images/?id=TRB-turboplinko_en_US&type=logo-square&version=1689797661198",
+  "https://client-int.qtlauncher.com/images/?id=TRB-bayraktar_en_US&type=logo-square&version=1689799195526",
+  "https://client-int.qtlauncher.com/images/?id=1x2-coinfield_en_US&type=logo-square&version=1667798993145",
+];
